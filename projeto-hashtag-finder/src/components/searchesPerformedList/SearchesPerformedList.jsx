3bd0e6ed64ddd1ce914917a3832e58b3dcfc8f82
ThiagoSilvaMorato <thiagosmorato@gmail.com>
2022-04-28 14:32:57 -0300

    Criado a lista estática de buscas realizadas

---
+++ projeto-hashtag-finder/src/components/searchesPerformedList/SearchesPerformedList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./header/Header";
+import Body from "./body/Body";
+
+import "./SearchesPerformedList.css";
+
+const SearchesPerformedList = () => {
+  return (
+    <div className='main-container'>
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+export default SearchesPerformedList;
