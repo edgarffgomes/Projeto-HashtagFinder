dc4203a31d9875cd81564b1ab5a204de8d1a6bc3
ThiagoSilvaMorato <thiagosmorato@gmail.com>
2022-05-02 14:54:20 -0300

    Feita a versão mobile da lista de busca

--- src/pages/search/SearchesPerformedList.jsx
+++ src/pages/search/SearchesPerformedList.jsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import Navbar from '../../components/navbar/Navbar';
-import Body from '../../components/searchesPerformedList/Body';
-import './SearchesPerformedList.css';
-
-/* se possivel utilize o css por modulo para evitar que outras partes da pagina
-sejam reescritas  e tome cuidado para chamar tags sem classe para n reescrever as outras de outras paginas..
-fora isso não encontrei problemas de sintaxe por aqui ja chamei a sua rota
-e o alexandre criou ela. Para visualizar a sua rota digite no navegador:
-http://localhost:3000/search
-
-Opcional  de uma olhada no mome das suas pastas e componentes e também na estrutura de pastas
-
- att joao
-*/
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Body from "../../components/searchesPerformedListBody/Body";
+import "./SearchesPerformedList.css";
 
 const SearchesPerformedList = () => {
   return (
-    <div className="bgNav">
+    <div className='bgNav'>
       <Navbar />
-      <div className="main-container">
+      <div className='main-container'>
         <Body />
       </div>
     </div>
