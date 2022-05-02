0632b9df10ee7ffd755d088d6e691540b6fda540
Edgar Gomes <edgargomes.iff@gmail.com>
2022-05-02 14:28:39 -0300

    Removendo conteúdo residual de mere mal-sucedida.

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../pages/home/Home';
-<<<<<<< HEAD
 import Login from '../pages/login/Login';
-=======
 import About from '../pages/about/About';
 import SearchesPerformedList from '../pages/search/SearchesPerformedList';
-// import Login from '../pages/login/Login'; com problema att joao
->>>>>>> main
 const MainRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
         <Route path="/login" element={<Login />}/>
-=======
         <Route path="/about" element={<About />} />
-        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login />} />
         <Route path="/search" element={<SearchesPerformedList />} />
-       
->>>>>>> main
       </Routes>
     </BrowserRouter>
   );
