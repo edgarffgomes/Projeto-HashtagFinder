81420ea3e7913448a4668f41cc22ba388d851266
Alexandre Junior <alexandrejunior408@gmail.com>
2022-05-06 17:09:00 -0300

    responsivo carrosel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -30,30 +30,37 @@ const Home = () => {
 
   /* configuração do carrosel */
   const settings = {
-    spaceBetween: 50,
+    spaceBetween: 1,
     slidesPerView: 5,
     navigation: true,
     pagination: {
       clickable: true,
     },
     breakpoints: {
+      1920: {
+        slidesPerView: 5,
+        width: 1920,
+        spaceBetween: 100,
+      },
       1112: {
+        slidesPerView: 3,
         width: 1112,
-        slidesPerView: 4,
+        spaceBetween: 100,
       },
       768: {
+        slidesPerView: 3,
         width: 768,
-        slidesPerView: 3,
+        spaceBetween: 200,
       },
-      480: {
-        spaceBetween: 150,
-        width: 100,
-        slidesPerView: 2,
-      },
-      414: {
-        spaceBetween: 14,
-        width: 'max-content',
+      481: {
         slidesPerView: 3,
+        width: 481,
+        spaceBetween: 200,
+      },
+      375: {
+        slidesPerView: 3,
+        width: 375,
+        spaceBetween: 185,
       },
     },
   };
