b20aa2b6e27a6873bacbfadba9083726422a5b23
Alexandre Junior <alexandrejunior408@gmail.com>
2022-05-11 13:36:54 -0300

    adiciona motion ao campo de busca

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,6 +9,8 @@ import Footer from '../../components/footer/Footer';
 // IMPORTED ICONS
 import iconAbout from '../../img/icon-info-circle.svg';
 import iconLogin from '../../img/icon-user-alt.svg';
+// MOTION
+import { motion } from 'framer-motion';
 // CARROUSEL SLIDER
 import { Slider, Slide } from '../../components/slider/ExportPattern';
 // TWEET CARDS
@@ -18,9 +20,11 @@ import { postData } from '../../api/AirtablePOST';
 
 const Home = () => {
   const [ativaNav, setAtivaNav] = useState(false); //navbar effect
-  const [searchResponse, setSearchResponse] = useState('');
-  const [searchValue, setSearchValue] = useState('');
-
+  const [searchResponse, setSearchResponse] = useState(''); //search answer
+  const [searchValue, setSearchValue] = useState(''); //field value
+  const [titleTag, setTitleTag] = useState();
+  const [resultsNumber, setResultsNumber] = useState(0);
+  const [animationMode, setAnimationMode] = useState(0);
   //navbar effect
   useEffect(function () {
     function posicaoScroll() {
@@ -88,6 +92,15 @@ const Home = () => {
         setSearchValue('');
       }
     }
+    if (e.keyCode === 8) {
+      setSearchResponse('');
+      setSearchValue('');
+      setTitleTag('');
+      setResultsNumber(0);
+    }
+    if (e.target.value.length >= 20) {
+      setSearchResponse('Limite de caracteres atingido! 🚨');
+    }
   }
 
   return (
@@ -128,7 +141,24 @@ const Home = () => {
           <div className={styles.bgSearch}>
             {/* background da imagem do campo de pesquisa */}
             <div className={styles.bgImageSearch}>
-              <img src={IconSearch} alt="" />
+              <img
+                src={IconSearch}
+                onClick={() => {
+                  setSearchValue(
+                    document
+                      .getElementById('input')
+                      .value.replace(/[^a-zA-Z0-9_]/g, '')
+                      .replace(' ', ''),
+                  );
+                  if (!document.getElementById('input').value.length) {
+                    setSearchResponse(
+                      'É necessario digitar algo no campo de buscas...',
+                    );
+                    setSearchValue('');
+                  }
+                }}
+                alt="iconSearch"
+              />
             </div>
             {/* background do input de pesquisa */}
             <div className={styles.bgInputSearch}>
@@ -141,6 +171,20 @@ const Home = () => {
               />
             </div>
           </div>
+          {searchResponse ? (
+            <>
+              <motion.div
+                initial={{ y: animationMode, opacity: 0 }}
+                animate={{ y: animationMode, opacity: 1 }}
+                onClick={() => setAnimationMode(animationMode)}
+                transition={{ duration: 0.5, delay: 0.4 }}
+              >
+                <div className={styles.bgResponse}>
+                  <div className={styles.textResponse}>{searchResponse}</div>
+                </div>
+              </motion.div>
+            </>
+          ) : null}
         </header>
         <main className={styles.bgMain}>
           <div className={styles.bgDisplaySearch}>
@@ -160,78 +204,9 @@ const Home = () => {
                   <span>@tweetusername</span>
                 </div>
               </Slide>
-              <Slide>
-                <div className={styles.bgImageGallery}>
-                  <img
-                    src="https://images.unsplash.com/photo-1595886802423-b92bba08046e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1936&q=80"
-                    alt="Teste"
-                    height="287px"
-                    width="287px"
-                  />
-                  <p>Postado por:</p>
-                  <span>@tweetusername</span>
-                </div>
-              </Slide>
-              <Slide>
-                <div className={styles.bgImageGallery}>
-                  <img
-                    src="https://images.unsplash.com/photo-1595886802423-b92bba08046e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1936&q=80"
-                    alt="Teste"
-                    height="287px"
-                    width="287px"
-                  />
-                  <p>Postado por:</p>
-                  <span>@tweetusername</span>
-                </div>
-              </Slide>
-              <Slide>
-                <div className={styles.bgImageGallery}>
-                  <img
-                    src="https://images.unsplash.com/photo-1595886802423-b92bba08046e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1936&q=80"
-                    alt="Teste"
-                    height="287px"
-                    width="287px"
-                  />
-                  <p>Postado por:</p>
-                  <span>@tweetusername</span>
-                </div>
-              </Slide>
-              <Slide>
-                <div className={styles.bgImageGallery}>
-                  <img
-                    src="https://images.unsplash.com/photo-1595886802423-b92bba08046e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1936&q=80"
-                    alt="Teste"
-                    height="287px"
-                    width="287px"
-                  />
-                  <p>Postado por:</p>
-                  <span>@tweetusername</span>
-                </div>
-              </Slide>
-              <Slide>
-                <div className={styles.bgImageGallery}>
-                  <img
-                    src="https://images.unsplash.com/photo-1595886802423-b92bba08046e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1936&q=80"
-                    alt="Teste"
-                    height="287px"
-                    width="287px"
-                  />
-                  <p>Postado por:</p>
-                  <span>@tweetusername</span>
-                </div>
-              </Slide>
             </Slider>
           </section>
           <section className={styles.flexCard}>
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
             <Card />
           </section>
         </main>
