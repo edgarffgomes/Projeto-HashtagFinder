cdcb335ba1a483e161f2f236047b46e88da4972e
ThiagoSilvaMorato <thiagosmorato@gmail.com>
2022-05-17 08:54:26 -0300

    Commit com ajuste na package.json para dar pull da main

---
+++ src/pages/search/SearchAPI.js
@@ -0,0 +1,28 @@
+import base from "../../utils/api";
+
+base("Buscas")
+  .select({
+    // Selecting the first 10 records in Grid view:
+    maxRecords: 10,
+    view: "Grid view",
+  })
+  .eachPage(
+    function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+
+      records.forEach(function (record) {
+        console.log("Retrieved", record.get("Squad"));
+      });
+
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+    },
+    function done(err) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    }
+  );
--- src/pages/search/SearchesPerformedList.jsx
+++ src/pages/search/SearchesPerformedList.jsx
@@ -1,34 +1,65 @@
-import React from 'react';
-import Navbar from '../../components/navbar/Navbar';
-import Body from '../../components/searchesPerformedListBody/Body';
-import './SearchesPerformedList.css';
+import React, { useEffect, useState } from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Body from "../../components/searchesPerformedListBody/Body";
+
+import "./SearchesPerformedList.css";
 
 /* ICONES DO NAVBAR */
-import iconLogout from '../../img/icon-power-off.svg';
-import iconHome from '../../img/icon-home.svg';
+import iconLogout from "../../img/icon-power-off.svg";
+import iconHome from "../../img/icon-home.svg";
+import base from "../../utils/api";
 
 const SearchesPerformedList = () => {
+  useEffect(() => {
+    base("Buscas")
+      .select({
+        // Selecting the first 10 records in Grid view:
+        maxRecords: 10,
+        view: "Grid view",
+      })
+      .eachPage(
+        function page(records, fetchNextPage) {
+          // This function (`page`) will get called for each page of records.
+
+          records.forEach(function (record) {
+            console.log(record.get("Squad"), record.get("Hashtag"));
+          });
+
+          // To fetch the next page of records, call `fetchNextPage`.
+          // If there are more records, `page` will get called again.
+          // If there are no more records, `done` will get called.
+          fetchNextPage();
+        },
+        function done(err) {
+          if (err) {
+            console.error(err);
+            return;
+          }
+        }
+      );
+  }, []);
+
   return (
-    <div className="bgNav">
+    <div className='bgNav'>
       <Navbar
         buttons={[
           {
-            route: '/',
+            route: "/",
             icon: iconHome,
-            title: 'home',
-            backgroundColor: ' #d842bd',
-            textColor: '#ffff',
+            title: "home",
+            backgroundColor: " #d842bd",
+            textColor: "#ffff",
           },
           {
-            route: '/login',
+            route: "/login",
             icon: iconLogout,
-            title: 'sair',
-            backgroundColor: ' #3634f7',
-            textColor: '#ffff',
+            title: "sair",
+            backgroundColor: " #3634f7",
+            textColor: "#ffff",
           },
         ]}
       />
-      <div className="main-container">
+      <div className='main-container'>
         <Body />
       </div>
     </div>
