cd8f56aec9dbabe6c84432d384f231f6778cea68
Alexandre Junior <alexandrejunior408@gmail.com>
2022-05-19 16:45:35 -0300

    adiciona api a página sobre e faz a listagem dos usuários

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -7,11 +7,50 @@ import Footer from '../../components/footer/Footer';
 /* ICONES DO NAVBAR */
 import iconLogin from '../../img/icon-user-alt.svg';
 import iconHome from '../../img/icon-home.svg';
+import iconGithub from '../../img/icon-github.svg';
+import iconMail from '../../img/icon-mail.svg';
+import iconLinkedin from '../../img/icon-linkedin.svg';
 import React, { useEffect, useState } from 'react';
 
 const About = () => {
   /* configuração do navbar */
   const [ativaNav, setAtivaNav] = useState(false);
+
+  const [dados, setDados] = useState([]);
+  const [texto, setTexto] = useState('');
+
+  useEffect(() => {
+    fetch(
+      'https://api.airtable.com/v0/app6wQWfM6eJngkD4/Projeto?view=Grid%20view&filterByFormula={Squad}=260422',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer key2CwkHb0CKumjuM',
+        },
+      },
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setTexto(data.records[0].fields.Sobre);
+      })
+      .catch((erro) => console.log(erro));
+
+    fetch(
+      'https://api.airtable.com/v0/app6wQWfM6eJngkD4/Equipe?view=Grid%20view&filterByFormula={Squad}=260422',
+      {
+        method: 'GET',
+        headers: {
+          Authorization: 'Bearer key2CwkHb0CKumjuM',
+        },
+      },
+    )
+      .then((response) => response.json())
+      .then(function (data) {
+        setDados(data.records);
+        console.log(data);
+      })
+      .catch((erro) => console.log(erro));
+  }, []);
 
   useEffect(function () {
     function posicaoScroll() {
@@ -54,16 +93,12 @@ const About = () => {
         <section className={styles.container}>
           <div className={styles.paragraph}>
             <h2 className={styles.paragraphTitle}>O que é</h2>
-            <p>
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
-              erat, sed diam voluptua. At vero eos et accusam et justo duo
-              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
-              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
-              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-              invidunt ut labore et dolore magna aliquyam erat, sed diam
-              voluptua. At vero eos et accusam et justo duo dolores
-            </p>
+            {/* como usar o dangerouslySetInnerHTML 
+            https://blog.logrocket.com/using-dangerouslysetinnerhtml-in-a-react-application/
+
+            Ele é usado pra renderizar um conteúdo dinâmico
+            */}
+            <p dangerouslySetInnerHTML={{ __html: texto }}></p>
           </div>
           <div className={styles.containerIllustration}>
             <img
@@ -74,10 +109,62 @@ const About = () => {
           </div>
         </section>
 
-        <Members />
+        <Members>
+          {dados.map((item) => (
+            <div key={item.id} className={styles.itemContainer}>
+              <div className={styles.item}>
+                <img
+                  className={styles.photo}
+                  src={item.fields.Imagem[0].url}
+                  alt="foto joão"
+                />
+                <span>
+                  <h3 className={styles.name}>{item.fields.Nome}</h3>
+                  <p className={styles.paragraphResume}>
+                    {item.fields['Descrição']}
+                  </p>
+                </span>
+                <div className={styles.socialIcons}>
+                  <a
+                    href={item.fields.Github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={iconGithub} alt="icone github" />
+                  </a>
+                  <a
+                    href={item.fields.Email}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={iconMail} alt="icone facebook" />
+                  </a>
+                  <a
+                    href={item.fields.LinkedIn}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={iconLinkedin} alt="icone linkedin" />
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Members>
       </main>
       <Footer />
     </>
   );
 };
 export default About;
+
+{
+  /* {dados.map((item) => (
+          <Members
+            key={item.id}
+            img={item.fields.Imagem[0].url}
+            name={item.fields.Nome}
+            text={item.fields['Descrição']}
+          />
+        ))} */
+}
